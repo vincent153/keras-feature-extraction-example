58bd804fb1739601949b95514fc55b66aaedc74a
vincent <vincent.ting1986@gmail.com>
2018-05-03 10:07:02 +0800

    add source

--- mnist_example.py
+++ mnist_example.py
@@ -4,7 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
-
+import cv2
 batch_size = 128
 num_classes = 10
 epochs = 5
@@ -14,28 +14,48 @@ img_rows, img_cols = 28, 28
 
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
+print(x_train.shape)
+print(y_train.shape)
 if K.image_data_format() == 'channels_first':
+    print('-'*50)    
+    print(x_train.shape[0])
+    print('-'*50)
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
     x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
     input_shape = (1, img_rows, img_cols)
 else:
+    print('-'*50)    
+    print(x_train.shape[0])
+    print('-'*50)
     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
     input_shape = (img_rows, img_cols, 1)
 
+
+for index in range(10):
+    #cv2.waitKey(0)
+    #cv2.imshow('image', x_train[index])
+    print(y_train[index])
+#cv2.destroyAllWindows()
+
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
+
+
+
 x_train /= 255
 x_test /= 255
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
+
 # convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
-
+print('type of class:{}'.format(type(y_train)))
+for i in range(10):
+    print(y_train[i])
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
@@ -51,7 +71,7 @@ model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
-
+'''
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
@@ -61,6 +81,6 @@ score = model.evaluate(x_test, y_test, verbose=0)
 model.save('mnist_test.h5')
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-
+'''
 
 
